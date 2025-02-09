85b5c159cbbdccef507aa4edc89fa18880cbf291
byorck <work-byorck@yandex.ru>
2025-02-09 16:23:45 +0300

    HW-2 complete

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private final String title;
     private int publicationYear;
@@ -25,4 +27,20 @@ public class Book {
         this.publicationYear = publicationYear;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, publicationYear, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "title='" + title + '\'' + ", publicationYear=" + publicationYear + ", author=" + author + '}';
+    }
 }
